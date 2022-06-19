e1e0ff081cd5e071e78fd0ad0baa8f4eee8ead44
dominikkilijan <dominikkilijan@gmail.com>
2022-06-19 10:55:18 +0200

    Measure time

--- main.m
+++ main.m
@@ -1,13 +1,20 @@
 clc;
 
-whichMethod = 1;
+whichMethod = 2;
 whichModel = 2;
-
+timeElapsed = 0;
 switch whichMethod
     case 1
+        tic;
         parity(whichModel);
+        timeElapsed = toc;
     case 2
+        tic;
         repetition(whichModel);
+        timeElapsed = toc;
     case 3
+        tic;
         % wywolanie CRC
+        timeElapsed = toc;
 end
+fprintf("Czas: %5.5f s\n", timeElapsed);
